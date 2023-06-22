3c343ad5d4e5e3ce8e007d471a968deef735d655
Muhammed Qureshi <119445359+MuhammedQureshi@users.noreply.github.com>
2023-06-22 18:10:11 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home() {
     axios.get(url).then((response) => {
       setWeather(response.data);
       // console.log(response.data);
-      
     });
     setCity('');
     setLoading(false);
